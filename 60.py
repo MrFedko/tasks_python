064791327340d8238d5fea6eaff2d9b2408d26c4
MrFedko <miklerst@mail.ru>
2022-01-22 20:32:34 +0300

    Составить частотный словарь элементов двумерного массива

---
+++ 60.py
@@ -0,0 +1,27 @@
+# Составить частотный словарь элементов двумерного массива
+
+from curses import keyname
+from multiprocessing.sharedctypes import Value
+import random
+from all_def import fill, print_ar
+
+m = 3
+array = [[0 for i in range(0, m)] for j in range(0, m)]
+
+def dictinary(any):
+    my_dict = {}
+    for i in range(0, len(any)):
+        for j in range(0, len(any[0])):
+            if any[i][j] not in my_dict.keys():
+                my_dict[any[i][j]] = 1
+            else:
+                my_dict[any[i][j]] = my_dict[any[i][j]] + 1                
+    return my_dict
+
+            
+fill(array)
+print_ar(array)
+result = dictinary(array)
+
+for i in result:
+    print(f'{i}: {result[i]}')
