58756a278e76dc54c8af71b35939975c2efa3a7f
MrFedko <miklerst@mail.ru>
2022-01-22 20:01:54 +0300

    def for all

--- all_def.py
+++ all_def.py
@@ -1,7 +1,9 @@
+import random
+
 def fill(any):
     for i in range(0, len(any)):
         for j in range(0, len(any[0])):
-            any[j][i] = random.randint(1, 9)
+            any[i][j] = random.randint(1, 9)
     return any
 
 def print_ar(any):
