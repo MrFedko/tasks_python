cf50147e5c63288ef98d5765c1b2632a04a46ac7
MrFedko <miklerst@mail.ru>
2022-01-30 10:59:25 +0300

    Дано число. Составить список чисел Фибоначчи, в том числе для отрицательных индексов

---
+++ new_task_list/task_26.py
@@ -0,0 +1,25 @@
+# Дано число. Составить список чисел Фибоначчи, в том числе для отрицательных индексов
+#  Т е для k = 5, список будет выглядеть так: [5, −3,  2, −1,  1, 0, 1, 1, 2, 3, 5] 0, 1, -1, 2, -3, 5
+
+n = int(input('введите натуральное число: '))
+
+
+def fibo(number) -> int:
+    fib =[0, 1]
+    for i in range(1, number - 1):
+        fib.append(fib[i] + fib[i - 1])
+    return fib
+
+def minus_fibo(number) -> int:
+    fib =[0, 1]
+    for i in range(1, number - 1):
+        fib.append(fib[i - 1] - fib[i])
+    fib.remove(0)
+    return fib
+       
+my_list = fibo(n)
+my_new_list = minus_fibo(n)
+my_new_list.reverse()
+my_new_list.extend(my_list)
+print(my_new_list)
+
