37bb0b2e165332b07a27b55a974f724e46b85928
MrFedko <miklerst@mail.ru>
2022-02-08 21:19:49 +0300

    Подсчитать сумму цифр в вещественном числе.

--- new_task_list/task_14.py
+++ new_task_list/task_14.py
@@ -1,19 +1,5 @@
 # Подсчитать сумму цифр в вещественном числе.
 
-def summ_of_float(number):
-    summa = 0
-    number = number.replace('.', '0')
-    for i in range(len(number)):
-        summa += int(number[i])
-    return summa
-
-def is_number(str):
-    try:
-        float(str)
-        return summ_of_float(str)
-    except ValueError:
-        return False
-
 n = input('Введите вещественное число: ')
-print(is_number(n))
-    
+summa = sum(list(map(int, n.replace('.', '0'))))
+print(summa)
