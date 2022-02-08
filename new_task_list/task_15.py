2bdc603e9cc6cc30c8147dffc1d260774fcbcf4e
MrFedko <miklerst@mail.ru>
2022-02-08 20:40:58 +0300

    Написать программу получающую набор произведений чисел от 1 до N.

--- new_task_list/task_15.py
+++ new_task_list/task_15.py
@@ -2,16 +2,13 @@
 # Пример: пусть N = 4, тогда
 # [ 1, 2, 6, 24 ]
 
+import operator
+from itertools import accumulate
 import random 
 
-
 def new_list(number):
-    my_list = [1, ]
-    for i in range(2, number + 1):
-        my_list.append(my_list[i - 2] * i)
-    return my_list
+    return list(accumulate([x for x in range(1, number + 1)], operator.mul))
 
 n = random.randint(2, 10)
 print(n)
 print(new_list(n))
-    
