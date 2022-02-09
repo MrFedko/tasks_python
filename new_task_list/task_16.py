15357775e5691b795d91a4aff1dbec37c8edfc03
MrFedko <miklerst@mail.ru>
2022-02-09 11:55:19 +0300

    Задать список из n чисел последовательности (1+1/n)**n и вывести на экран их сумму

--- new_task_list/task_16.py
+++ new_task_list/task_16.py
@@ -4,17 +4,8 @@ from itertools import accumulate
 
 n = int(input('Введите сколько чисел последовательности вывести на экран: '))
 
-def sequence(number):
-    my_list = []
-    for i in range(1, number + 1):
-        my_list.append((1 + 1/i) ** i)
-    return my_list
-
 my_func = lambda _, x: ((1 + 1/x) ** x)
-my_list = list(accumulate([x for x in range(1, n + 1)], my_func, initial = 1))
-my_list = my_list[1:]
+my_list = list(accumulate([x for x in range(1, n + 1)], my_func, initial = 1))[1:]
 
-print(*sequence(n))
-print(sum(sequence(n)))
 print(*my_list)
 print(sum(my_list))
