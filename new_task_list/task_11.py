2aa92a4ff7f2e2d2b7528272a5b1e190baa777db
MrFedko <miklerst@mail.ru>
2022-02-09 11:52:36 +0300

    Для натурального N создать список: 1, -3, 9, -27, 81 и т.д.

--- new_task_list/task_11.py
+++ new_task_list/task_11.py
@@ -1,14 +1,14 @@
 # Для натурального N создать список: 1, -3, 9, -27, 81 и т.д.
 
+from itertools import accumulate
+import math
 n = int(input('Введите сколько чисел последовательности вывести на экран: '))
 
-def sequence(number):
-    my_list = [1, ]
-    for i in range(1, number):
-        if my_list[i - 1] > 0:
-            my_list.append(-3**i)
-        else:
-            my_list.append(3**i)
-    return my_list
+def odd(_, any):
+    if math.log(any, 3) % 2 != 0:
+        any *= - 1
+    return any
 
-print(*sequence(n))
+my_new_list = list(accumulate([3 ** x for x in range(n)], odd))
+
+print(*my_new_list)
