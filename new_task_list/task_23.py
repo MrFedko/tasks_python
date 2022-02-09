f7fe8569dd82482810bc1d166c7c9733cf234d2a
MrFedko <miklerst@mail.ru>
2022-02-09 12:41:57 +0300

    Найти произведение пар чисел в списке. Парой считаем первый и последний элемент, второй и предпоследний и т.д.

--- new_task_list/task_23.py
+++ new_task_list/task_23.py
@@ -4,21 +4,14 @@
 import random
 
 def fill(any):
-    new_list = []
-    for i in range(any):
-            new_list.append(random.randint(1, 9))
-    return new_list
+    return [random.randint(1,9) for x in range(any)]
 
 def product_par(any_list):
     new_list = []
-    if len(any_list) % 2 != 0:
-        for i in range(0, round((len(any_list) - 1) / 2)):
-            new_list.append(any_list[i] * any_list[(i + 1) * (-1)])
-        new_list.append(any_list[round((len(any_list) - 1 )/ 2)])
-    else:
-        for i in range(0, round(len(any_list) / 2)):
-            new_list.append(any_list[i] * any_list[(i + 1) * (-1)])
+    for i in range(round(len(any_list) / 2)):
+        new_list.append(any_list[i] * any_list[-(i+1)])
     return new_list
         
 my_list = fill(11)
 print(f'{my_list} \n {product_par(my_list)}')
+
