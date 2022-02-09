6df8d3c6cfa27c79f34cca3a838121c50f5dc4d0
MrFedko <miklerst@mail.ru>
2022-02-09 12:19:59 +0300

    Найти сумму чисел списка стоящих на нечетной позиции

--- new_task_list/task_22.py
+++ new_task_list/task_22.py
@@ -3,16 +3,10 @@
 import random
 
 def fill(any):
-    new_list = []
-    for i in range(any):
-            new_list.append(random.randint(1, 9))
-    return new_list
+    return [random.randint(1,9) for x in range(any)]
 
 def summ_odd(any_list):
-    summa = 0
-    for i in range(1, len(any_list), 2):
-        summa += any_list[i]
-    return summa
+    return sum(any_list[1::2])
 
 my_list = fill(10)
 print(my_list, summ_odd(my_list))
